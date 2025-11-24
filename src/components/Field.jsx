fafcc22bceaad7754d55bd12d546845743193f61
Kino De Torres <kendetorres2105@gmail.com>
2025-11-24 18:44:54 +0800

    Add editable field

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,8 +1,8 @@
-export default function Field({name, prompt}){
+export default function Field({ name, prompt }) {
     return (<>
-    <div className="text-left self-start" >{name} </div>  
-    <div className="bg-background text-gray-400 p-1.5 rounded-md m-3 w-full">
-    {prompt}
-    </div>
+        <div className="text-left self-start" >{name} </div>
+        <div className="bg-background p-1.5 rounded-md m-3 w-full">
+            < input type="text" placeholder={prompt} className="text-gray-400 placeholder-gray-400 placeholder-shown:text-gray-400  not-placeholder-shown:text-black w-full" />
+        </div>
     </>)
 }
