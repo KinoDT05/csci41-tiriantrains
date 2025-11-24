de463fa87161ca5821cd02ab8827519ffc79b100
Kino De Torres <kendetorres2105@gmail.com>
2025-11-24 21:18:08 +0800

    complete train maintenance page

--- src/app/Maintenance/Train/[id]/page.js
+++ src/app/Maintenance/Train/[id]/page.js
@@ -6,41 +6,46 @@ import prisma from "../../../../../lib/prisma";
 
 const columns = [
   { key: "date", label: "Date" },
-  { key: "crew", label: "Crew In Charge" },
+  { key: "crew", label: "Crew" },
+  { key: "crewInCharge", label: "Crew In Charge" },
   { key: "task", label: "Task" },
   { key: "condition", label: "Condition" },
 ];
 
-export default async function Train() {
+export default async function Train({ params }) {
+    const { id } = await params; // await here
+
+    const trainIDParam = parseInt(id);
+
+    console.log(params.id);
     const trainDetails = await prisma.train.findUniqueOrThrow({
         where: {
-            trainID: 1
+            trainID: trainIDParam
         },
+        include: {
+            trainModel: true
+        }
     });
 
     const maintenance = await prisma.maintenanceHistory.findMany({
         where: {
-            maintainedTrainID: 1
+            maintainedTrainID: trainIDParam
         },
-    });
-
-    console.log(maintenance)
-
-    const model = await prisma.trainModel.findUniqueOrThrow({
-        where: {
-            modelID: trainDetails.trainModelID
+        include: {
+            crewAssigned: true
         }
     });
 
     const data = maintenance.map(t => ({
-        date: `${t.date.toString()}`,
-        crew: `${t.crewAssigned.toString()}`,
-        task: `${t.task.toString()}`,
+        date: t.date.toISOString().split("T")[0],
+        crew: `${t.maintainer}`,
+        crewInCharge: `${t.crewAssigned.inCharge}`,
+        task: `${t.task}`,
         condition: `${t.condition}`,
     }));
 
     const trainID = `T-${trainDetails.trainID.toString().padStart(4, '0')}`;
-    const trainModelID = `${model.trainType}-${model.modelID.toString().padStart(3, '0')}`;
+    const trainModelID = `${trainDetails.trainModel.trainType}-${trainDetails.trainModel.modelID.toString().padStart(3, '0')}`;
     return (
     <div className="my-10">
 
@@ -51,21 +56,21 @@ export default async function Train() {
 
             {/* Replace this with a map */}
                   <Attribute name="Model" value={trainModelID} />
-                    <Attribute name="Max Speed (kph)" value={model.maxSpeed} />
-                    <Attribute name="No. of Seats" value={model.noSeats} />
-                    <Attribute name="No. of Toilets" value={model.noToilets } />
+                    <Attribute name="Max Speed (kph)" value={trainDetails.trainModel.maxSpeed} />
+                    <Attribute name="No. of Seats" value={trainDetails.trainModel.noSeats} />
+                    <Attribute name="No. of Toilets" value={trainDetails.trainModel.noToilets } />
           </div>
           
       </div>  
       
       <div className="flex ">
       {/* Replace this with a map */}
-                <BoolAttribute name="Reclining Seats" bool={model.hasRecliningSeats} />  
-                <BoolAttribute name="Folding Table" bool={model.hasFoldingTables} />  
-        <BoolAttribute name="Disability Access" bool={model.hasDisabilityAccess} />  
-        <BoolAttribute name="Luggage Access" bool={model.LuggageStorage} />  
-        <BoolAttribute name="Vending Machine" bool={model.hasVendingMachine} />  
-        <BoolAttribute name="Food Service" bool={model.hasFoodService} />  
+                <BoolAttribute name="Reclining Seats" bool={trainDetails.trainModel.hasRecliningSeats} />  
+                <BoolAttribute name="Folding Table" bool={trainDetails.trainModel.hasFoldingTables} />  
+                <BoolAttribute name="Disability Access" bool={trainDetails.trainModel.hasDisabilityAccess} />  
+                <BoolAttribute name="Luggage Access" bool={trainDetails.trainModel.LuggageStorage} />  
+                <BoolAttribute name="Vending Machine" bool={trainDetails.trainModel.hasVendingMachine} />  
+                <BoolAttribute name="Food Service" bool={trainDetails.trainModel.hasFoodService} />  
       </div>
       
       <Table columns={columns} data={data} />
