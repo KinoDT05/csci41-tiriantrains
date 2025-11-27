c9840d852da66352c7aac3355f2cf0c486816bac
Kino De Torres <kendetorres2105@gmail.com>
2025-11-27 14:21:51 +0800

    update prisma client

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 
-export const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+const prisma = globalThis.prisma || new PrismaClient({
+    log: ["error"], 
+});
+
+if (process.env.NODE_ENV !== "production") {
+    globalThis.prisma = prisma;
+}
+
+export { prisma };
